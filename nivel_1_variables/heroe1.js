53cbb68e189820be92ad031d32254b09d23f0ec5
JesseAU <zaiduriarteleo@gmail.com>
2026-03-03 10:30:04 -0500

    Estudiante A: Agrega heroe1.js con variables de Batman

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,12 @@
+// Heroe: Batman
+// Creado por: Estudiante A
+
+let nombreHeroe = "Batman";
+let poder = "Inteligencia y Gadgets";
+let nivelEnergia = 100;
+let ciudad = "Gotham City";
+let enemigo = "The Joker";
+
+console.log(nombreHeroe + " tiene " + nivelEnergia + " de energía y está defendiendo " + ciudad + ".");
+console.log("Su mayor enemigo actual es " + enemigo + ".");
+console.log("Batman usa su " + poder + " para patrullar las calles.");
---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,12 @@
+// Heroe: Spider-Man
+// Creado por: Estudiante B
+
+let nombreHeroe = "Spider-Man";
+let poder = "Sentido Arácnido y Telarañas";
+let nivelEnergia = 85;
+let ciudad = "New York";
+let enemigo = "Green Goblin";
+
+console.log(nombreHeroe + " tiene " + nivelEnergia + " de energía y está protegiendo " + ciudad + ".");
+console.log("Se enfrenta a su archienemigo " + enemigo + ".");
+console.log("Gracias a su " + poder + ", puede balancearse entre los edificios.");
