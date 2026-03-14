87a2871a763017e3b7880d0ec5d2accf6f7e9f02
JesseAU <zaiduriarteleo@gmail.com>
2026-03-14 10:50:36 -0500

    Completa evidencia y documentación final del Día 8

---
+++ dia1_fase1_variables/heroe2.js
@@ -0,0 +1,10 @@
+// El buen vecino Spider-Man
+let nombreHeroe = "Spider-Man";
+let poder = "Sentido Arácnido y Telarañas";
+let nivelEnergia = 95; // Recuperó fuerzas descansando
+let ciudad = "New York";
+let enemigo = "Green Goblin";
+
+console.log(nombreHeroe + " tiene " + nivelEnergia + " de energía y está protegiendo " + ciudad + ".");
+console.log("Se enfrenta a su archienemigo " + enemigo + ".");
+console.log("Gracias a su " + poder + ", puede balancearse entre los edificios.");
