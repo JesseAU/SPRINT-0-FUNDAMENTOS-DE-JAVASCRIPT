27fe2140febd9683fc6e199bbae70afcf4cd7a28
JesseAU <zaiduriarteleo@gmail.com>
2026-04-07 12:15:42 -0500

    TS: Limpieza de archivos de JS/Node. Rama exclusiva de TypeScript

--- dia8/restaurante-backend/restaurante-backend/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-// middlewares/logger.js
-const logger = (req, res, next) => {
-    const ahora = new Date().toLocaleTimeString();
-    console.log(`[${ahora}] ${req.method} ${req.url}`);
-    next(); // SIN next() LA PETICIÓN SE DETIENE AQUÍ
-};
-
-module.exports = logger;
